4e2231fe538cd370a3ab0c3cb792da4ea8d9c7cc
Cameron Cox <cameronacox0904@gmail.com>
2024-04-09 13:35:58 -0500

    Added beta user authentication and account creation, and a basic home page.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: redirect('/home')
+  get 'home', to: 'pages#home', as: 'home'
+  
 end
