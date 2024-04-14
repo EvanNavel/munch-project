c225f593cac2cb27e2c69a713e2fa76fd1bcbe2c
Alexander Fiodorov <alexfiodorov02@gmail.com>
2024-04-13 20:02:05 -0500

    Set up posts as root page (to follow wireframe), fixed typo

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,10 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  
+
   # Defines the root path route ("/")
   # root "articles#index"
-  root to: redirect('/home')
-  get 'home', to: 'pages#home', as: 'home'
+  root to: redirect('/posts')
   get 'posts', to: 'posts#index', as: 'user_posts'
   get 'posts/:id',to: 'posts#show', as: 'show_posts'
 end
