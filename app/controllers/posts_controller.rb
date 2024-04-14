6a210c77c3a32527b3ccafe11c0504d25edefbb7
Alexander Fiodorov <alexfiodorov02@gmail.com>
2024-04-13 21:27:30 -0500

    Fixed controller - adding posts is now working

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,15 +16,45 @@ class PostsController < ApplicationController
     end
 
     def create
-        @post = Post.new(params.require(:post).permit(:title, :body, :meal, :difficulty, :cuisine))
+        @post = current_user.posts.build(params.require(:post).permit(:title, :body, :meal, :difficulty, :cuisine))
         if @post.save
-            flash[:success] = 'New post successfully added!'
-            redirect_to posts_url
+          flash[:success] = 'Blog post was successfully created.'
+          redirect_to posts_path(@post)
         else
-            puts @post.errors.full_messages
-            flash.now[:error] = 'Post creation failed'
-            render :new, status: :unprocessable_entity
+          flash.now[:error] = 'Unable to create blog post.'
+          render :new, status: :unprocessable_entity
         end
-    end
+      end
+
+    def edit
+        @post = Post.find(params[:id])
+        render :edit
+      end
+
+      def update
+        @post = Post.find(params[:id])
+        if @post.update(params.require(:post).permit(:title, :body, :meal, :difficulty, :cuisine))
+          flash[:success] = 'Post was successfully updated.'
+          redirect_to post_path(@post)
+        else
+          flash.now[:error] = 'Unable to update post.'
+          render :edit, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+        flash[:success] = 'Post was successfully destroyed.'
+        redirect_to posts_url
+      end
+
+      def require_permission
+        @user = User.find(params[:user_id])
+        if @user.creator != current_user
+          flash[:error] = 'You do not have permission to do that.'
+          redirect_to user_path(@user)
+        end
+      end
 
 end
