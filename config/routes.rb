530d0fae6798c3734c335d0d5f8bed3e6e826c82
Mohammad Hujeir <mhujeir@memphis.edu>
2024-04-14 07:51:07 -0500

    Committing changes to config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,34 @@ Rails.application.routes.draw do
   root to: redirect('/home')
   get 'home', to: 'pages#home', as: 'home'
   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  #Commenting
+  get '/recipes/:recipe_id/comments', to: 'comments#index', as: 'recipe_comments'
+  post '/recipes/:recipe_id/comments', to: 'comments#create'
+  patch '/comments/:id', to: 'comments#update', as: 'update_comment'
+
+  #Flagging
+    patch 'recipes/:id/flag', to: 'recipes#flag', as: 'flag_recipe'
+    patch 'recipes/:id/unflag', to: 'recipes#unflag', as: 'unflag_recipe'
+  
+  
 end
