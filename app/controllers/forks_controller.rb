a87966c9f97bceb0405f3fca3080d17645a1f85d
Robert Evan Patterson <mechapoltergeist333@gmail.com>
2024-04-26 21:44:24 -0500

    all functions added back to fork page, bugs with comments and flags

--- app/controllers/forks_controller.rb
+++ app/controllers/forks_controller.rb
@@ -1,5 +1,6 @@
 class ForksController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:show]
+  before_action :set_fork, only: [:show, :edit, :update, :destroy]
 
   def new
     @post = Post.find(params[:post_id])
@@ -9,6 +10,9 @@ class ForksController < ApplicationController
   def show
     @fork = Fork.find(params[:id])
     @parent = @fork
+    @commentable = @fork
+    @comment = Comment.new
+    render :show
   end
 
   def create
@@ -19,11 +23,31 @@ class ForksController < ApplicationController
     if @fork.save
       redirect_to post_fork_path(@post, @fork), notice: 'Recipe forked successfully.'
     else
-      render :new, notice: 'Failed to create new post from forked recipe.'
+      render :new, alert: 'Failed to create new post from forked recipe.'
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @fork.update(fork_params)
+      redirect_to [@fork.post, @fork], notice: 'Fork was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @fork.destroy
+    redirect_to posts_path, notice: 'Fork was successfully deleted.'
+  end
+
   private
+
+  def set_fork
+    @fork = Fork.find(params[:id])
+  end
 
   def fork_params
     params.require(:fork).permit(:title, :body, :meal, :difficulty, :cuisine, :image)
