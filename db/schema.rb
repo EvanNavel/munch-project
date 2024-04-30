f4541edf10448d2fdd28624b2be25f3af86410b5
Robert Evan Patterson <mechapoltergeist333@gmail.com>
2024-04-29 22:31:54 -0500

    updated seed data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_30_030108) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_30_032615) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -118,6 +118,9 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_30_030108) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.integer "flags_count"
+    t.text "description"
+    t.text "ingredients"
+    t.text "directions"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
